792789f12b44d92ef73f9523804035f6e698244f
Yasir Azizi <yasir720@my.yorku.ca>
2021-01-17 21:58:45 -0500

    added in all the needed packages

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export const Landing = () => {
     return (
-        <div className="text-6xl font-mono">Landing</div>
+        <div className="  text-blue-500 text-6xl font-mono">Landing</div>
     )
 }

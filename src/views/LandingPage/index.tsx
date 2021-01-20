f1bab9b94429392bde9ac88a89a3b35e74546767
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-01-20 12:57:25 -0500

    home page change v1

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -3,6 +3,23 @@ import React from 'react';
 
 export const Landing = () => {
     return (
-        <div className="text-blue-500 text-6xl font-mono">Landing</div>
+        <div>
+            <div className="bg-gray-300 min-h-screen flex flex-col p-8 justify-center">
+                <div className="font-extrabold text-center text-lg"> Music Tab</div>
+                    <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white">
+                
+                </div>
+
+                <div className="container max-w-md mx-auto p-1 text-center background flex-row ">
+                    <button className="bg-green-500">
+                        import new music tab!
+                    </button>
+                    <button className="bg-red-600">
+                            reset
+                    </button>
+                </div>
+            </div>
+        </div>
+        
     )
 }

4d1555a84cf249e8a9b645b298b0cce8f7b64465
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-01-20 12:46:40 -0500

    Lab1 change AJ

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		System.out.println("Hello world!");
 		System.out.println("Hello world From Muhammad Azizi! Howdy");
 		System.out.println("Hello world From Daniel Santorelli!");
-		System.out.println("Hello world From Arjit Johar!");
+		System.out.println("Hello world From Arjit Johar! Howdy");
 
 		System.out.println("This is a push - Andrew");
 		System.out.println("this is whatever -alp");

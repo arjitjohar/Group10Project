19a59833e9160d0dd9ec99f05298993bfd4852f0
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-01-20 13:24:53 -0500

    added the buttons to the home page

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 
 export const Landing = () => {
+
+    function import_new_music()
+    {
+        return setCount(count+1)
+    }
+
+    function reset(){
+        return setCount(0)
+    }
+
+    function copy_paste(){
+        return setCount(99)
+    }
+
+    const [count, setCount] = useState(0);
+
+    
+
     return (
         <div>
             <div className="bg-gray-300 min-h-screen flex flex-col p-8 justify-center">
                 <div className="font-extrabold text-center text-lg"> Music Tab</div>
                     <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white">
-                
                 </div>
 
                 <div className="container max-w-md mx-auto p-1 text-center background flex-row ">
-                    <button className="bg-green-500">
-                        import new music tab!
+                    <button onClick={() => import_new_music()} className="bg-green-500">
+                        import from local storage {count}
                     </button>
-                    <button className="bg-red-600">
-                            reset
+
+
+                    <button onClick={() => copy_paste()} className="background bg-purple-400">
+                        copy paste from computer
+                    </button>
+                    
+                    <button onClick={() => reset()} className="background bg-red-400">
+                        reset
                     </button>
                 </div>
             </div>

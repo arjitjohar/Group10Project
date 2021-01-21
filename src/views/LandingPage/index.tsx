5fec4481288cb765c76e37396b4d5d589ebc4904
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-01-21 16:13:58 -0500

    buttons made

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -25,22 +25,16 @@ export const Landing = () => {
         <div>
             <div className="bg-gray-300 min-h-screen flex flex-col p-8 justify-center">
                 <div className="font-extrabold text-center text-lg"> Music Tab</div>
-                    <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white">
-                </div>
+                    <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white bg">
+                        
+                    </div>
 
-                <div className="container max-w-md mx-auto p-1 text-center background flex-row ">
-                    <button onClick={() => import_new_music()} className="bg-green-500">
-                        import from local storage {count}
-                    </button>
-
-
-                    <button onClick={() => copy_paste()} className="background bg-purple-400">
-                        copy paste from computer
-                    </button>
-                    
-                    <button onClick={() => reset()} className="background bg-red-400">
-                        reset
-                    </button>
+                <div className="max-w-md w-full mx-auto">
+                    <div className="flex md:space-x-2 justify-center">
+                        <button  className="w-20 align-center border border-green-400 bg-green-300 rounded-lg">1</button>
+                        <button className="w-20 align-center border border-blue-400 bg-blue-300 rounded-lg">2</button>
+                        <button className="w-20 align-center border border-red-400 bg-red-300 rounded-lg">3</button>
+                    </div>
                 </div>
             </div>
         </div>

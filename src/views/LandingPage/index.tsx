4cac62bb6ed24c9ea32e4cf0e15667db8490c404
Yasir Azizi <yasir720@my.yorku.ca>
2021-01-25 11:57:30 -0500

    Update index.tsx

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -4,5 +4,7 @@ import React from 'react';
 export const Landing = () => {
     return (
         <div className="text-blue-500 text-6xl font-mono">Landing</div>
+        <div className="text-blue-500 text-6xl font-mono">Page</div>
+        <div className="text-blue-500 text-6xl font-mono">Nice</div>
     )
 }

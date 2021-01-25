39212b4865887087c049152e6fef9898aa5cc801
dsantor9 <dannyboy9901@rogers.com>
2021-01-25 12:09:34 -0500

    lab 2 practice - fantastico

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -5,6 +5,6 @@ export const Landing = () => {
     return (
         <div className="text-blue-500 text-6xl font-mono">Hello</div>
         <div className="text-blue-500 text-6xl font-mono">Page</div>
-        <div className="text-blue-500 text-6xl font-mono">Nyce</div>
+        <div className="text-blue-500 text-6xl font-mono">Fantastico</div>
     )
 }

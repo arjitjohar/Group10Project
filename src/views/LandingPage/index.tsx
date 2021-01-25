178dfb637935ebbc506058afd5164134d50c301f
Yasir Azizi <yasir720@my.yorku.ca>
2021-01-25 11:58:15 -0500

    Update index.tsx

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -5,6 +5,6 @@ export const Landing = () => {
     return (
         <div className="text-blue-500 text-6xl font-mono">Landing</div>
         <div className="text-blue-500 text-6xl font-mono">Page</div>
-        <div className="text-blue-500 text-6xl font-mono">Nice</div>
+        <div className="text-blue-500 text-6xl font-mono">Nyce</div>
     )
 }

cf1553727b2d900e6458b7f759924db7ddb9d350
Andrew <ngov.andrew@gmail.com>
2021-01-25 11:58:44 -0500

    Update index.tsx

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Landing = () => {
     return (
-        <div className="text-blue-500 text-6xl font-mono">Landing</div>
+        <div className="text-blue-500 text-6xl font-mono">Hello</div>
         <div className="text-blue-500 text-6xl font-mono">Page</div>
         <div className="text-blue-500 text-6xl font-mono">Nice</div>
     )

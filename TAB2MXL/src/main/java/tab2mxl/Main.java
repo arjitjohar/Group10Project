0b823e4e11309bc776e197d745ff098634dfd6ae
Andrew <ngov.andrew@gmail.com>
2021-01-27 12:51:49 -0500

    Lab2 Andrew Ngov

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -22,7 +22,7 @@ public class Main {
 		System.out.println("big poop and pee");
 		System.out.println("LAB2 PRACTICE DONE");
 		System.out.println("lab2 but alp");
-		System.out.println("LAB2");
+		System.out.println("LAB2 Andrew N");
 
 
 	}

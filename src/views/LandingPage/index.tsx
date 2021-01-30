6790592298b0a6f32cec10b601356dbfb438f570
arjitjohar <arjit39@my.yorku.ca>
2021-01-30 09:13:27 -0500

    added text input field

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -12,96 +12,73 @@ export const Landing = () => {
   let ShowModalCSS = "None";
   function toggleModal() {
     setShowModal(!showModal);
-    if(showModal){
+    if (showModal) {
       ShowModalCSS = "";
     } else {
       ShowModalCSS = "None";
     }
   }
 
-  function closeModal(){
+  function closeModal() {
     setShowModal(false)
-    ShowModalCSS="None"
+    ShowModalCSS = "None"
   }
 
-//implement function to close modal
+  //implement function to close modal
+  
 
-//implement lorem ipsum text
+
+  }
 
 
-    function clicked_new_text()
-            {
-                  <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center">
-                  <div className=" bg-gray-200">
-                    <div className="flex justify-between items-center ">
-                        <h4 className="text-lg font-bold"> confirm delete?</h4>
-                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                    </div> 
-                    <div className="mt-2">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                          Voluptatum animi autem adipisci quos! Libero blanditiis 
-                          sunt itaque ducimus dolor optio possimus odio error 
-                          iusto commodi accusamus, vitae dolore debitis facilis.</p>  
-                    </div>
-                    <div className="mt=3 flex justify-end space-x-3">
-                      <button>cancel</button>
-                      <button>delete</button>
-                    </div>
-                  </div>
-              </div>
-              
-              
-            }
 
-    
+  return (
+    <div>
+      <div className="bg-gray-300 min-h-screen flex flex-col p-8 justify-center">
+        <div className="font-extrabold text-center text-lg"> Music Tab</div>
+        <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white bg">
 
-    return (
-        <div>
-            <div className="bg-gray-300 min-h-screen flex flex-col p-8 justify-center">
-                <div className="font-extrabold text-center text-lg"> Music Tab</div>
-                    <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white bg">
-                        
-                    </div>
+        </div>
 
-                <div className="max-w-md w-full mx-auto">
-                    <div className="flex md:space-x-2 justify-center">
-                        <button className="w-15 hover:bg-green-400 transition duration-100 align-center border border-green-400 bg-green-300 rounded-lg font-medium px-4">import</button>
-                        <Button className="w-15 hover:bg-blue-400 transition duration-100 align-center border border-blue-400 bg-blue-300 rounded-lg font-medium px-4" variant="primary" onClick={toggleModal}>paste</Button>                        
-                        <button className="w-15 hover:bg-red-400 transition duration-100 align-center border border-red-400 bg-red-300 rounded-lg font-medium px-4">reset</button>
-                        <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center" style={(showModal) ? {display: ""} : {display: "None"}}>
-                        <div className=" bg-gray-200">
-                         <div className="flex justify-between items-center ">
-                          <h4 className="text-lg font-bold"> Copy and Paste Text!</h4>
-                          <button type="button" className="hover:bg-blue-300 transition duration-500 " onClick={closeModal}>
-                          <svg className="w-5 h-5 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                          </button>
-                       </div> 
-                        <div className="mt-2">
-                        
-                              
-                    </div>
-                    <div className="mt=3 flex justify-end space-x-3">
-                      <button className="w-20 hover:bg-gray-400 transition duration-100 align-center border border-gray-400 bg-gray-300 rounded-lg font-medium">accept</button>
-                    </div>
-                  </div>
-                  
-              </div>
-                    
-
-                        
-                    </div>
-
-                    
+        <div className="max-w-md w-full mx-auto">
+          <div className="flex md:space-x-2 justify-center">
+            <button className="w-15 hover:bg-green-400 transition duration-100 align-center border border-green-400 bg-green-300 rounded-lg font-medium px-4">import</button>
+            <Button className="w-15 hover:bg-blue-400 transition duration-100 align-center border border-blue-400 bg-blue-300 rounded-lg font-medium px-4" variant="primary" onClick={toggleModal}>paste</Button>
+            <button className="w-15 hover:bg-red-400 transition duration-100 align-center border border-red-400 bg-red-300 rounded-lg font-medium px-4">reset</button>
+            <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center" style={(showModal) ? { display: "" } : { display: "None" }}>
+              <div className=" bg-gray-200 p-30">
+                <div className="flex justify-between items-center ">
+                  <h4 className="text-lg font-bold"> Copy and Paste Text!</h4>
+                  <button type="button" className="hover:bg-blue-300 transition duration-500 " onClick={closeModal}>
+                    <svg className="w-5 h-5 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                  </button>
                 </div>
+                <div className="mt-2">
+                    <textarea rows={4} cols={50}></textarea>
+                </div>
+                <div className="mt=3 flex justify-end space-x-3">
+                    <button className="w-20 hover:bg-gray-400 transition duration-100 align-center border border-gray-400 bg-gray-300 rounded-lg font-medium">accept</button>
+                  </div>
+
+              </div>
+
             </div>
 
 
 
-            
-            
-        </div>
-        
+          </div>
 
-    )
-    
+
+        </div>
+      </div>
+
+
+
+
+
+    </div>
+
+
+  )
+
 }

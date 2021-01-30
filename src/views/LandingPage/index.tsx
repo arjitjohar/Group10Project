c966b5d0367ab270bf4f6e5d91bf2b90e933178b
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-01-30 00:41:13 -0500

    addition of open and close button

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -1,61 +1,32 @@
 
 import React, { useState, Component, FunctionComponent, FC } from 'react';
 //import Modal from 'react-bootstrap/Modal';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap'
 
 
-
-
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-type CatProps = {
-  mouse: MouseState
-}
-
-type MouseState = {
-  x: number,
-  y: number
-}
-
-export class Cat extends Component<CatProps> {
-  render() {
-    const { mouse } = this.props;
-    return (
-      <img src="cat.gif" style={ { position: 'absolute', left: mouse.x, top: mouse.y } } />
-    );
-  }
-}
 
 
 export const Landing = () => {
-     const [modalShow, setModalShow] = React.useState(false);
-    const toggleModal = () => setModalShow(!modalShow);
+  const [showModal, setShowModal] = React.useState(false);
 
+  let ShowModalCSS = "None";
+  function toggleModal() {
+    setShowModal(!showModal);
+    if(showModal){
+      ShowModalCSS = "";
+    } else {
+      ShowModalCSS = "None";
+    }
+  }
+
+  function closeModal(){
+    setShowModal(false)
+    ShowModalCSS="None"
+  }
+
+//implement function to close modal
+
+//implement lorem ipsum text
 
 
     function clicked_new_text()
@@ -94,17 +65,42 @@ export const Landing = () => {
 
                 <div className="max-w-md w-full mx-auto">
                     <div className="flex md:space-x-2 justify-center">
-                        <button className="w-20 hover:bg-green-400 align-center border border-green-400 bg-green-300 rounded-lg font-medium">random button</button>
-                        <Button className="w-20 hover:bg-blue-400 align-center border border-blue-400 bg-blue-300 rounded-lg font-medium" variant="primary" onClick={toggleModal}> Launch demo modal</Button>                        
-                        <button className="w-20 hover:bg-red-400 align-center border border-red-400 bg-red-300 rounded-lg font-medium">random button</button>
+                        <button className="w-15 hover:bg-green-400 transition duration-100 align-center border border-green-400 bg-green-300 rounded-lg font-medium px-4">import</button>
+                        <Button className="w-15 hover:bg-blue-400 transition duration-100 align-center border border-blue-400 bg-blue-300 rounded-lg font-medium px-4" variant="primary" onClick={toggleModal}>paste</Button>                        
+                        <button className="w-15 hover:bg-red-400 transition duration-100 align-center border border-red-400 bg-red-300 rounded-lg font-medium px-4">reset</button>
+                        <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center" style={(showModal) ? {display: ""} : {display: "None"}}>
+                        <div className=" bg-gray-200">
+                         <div className="flex justify-between items-center ">
+                          <h4 className="text-lg font-bold"> Copy and Paste Text!</h4>
+                          <button type="button" className="hover:bg-blue-300 transition duration-500 " onClick={closeModal}>
+                          <svg className="w-5 h-5 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                          </button>
+                       </div> 
+                        <div className="mt-2">
+                        
+                              
+                    </div>
+                    <div className="mt=3 flex justify-end space-x-3">
+                      <button className="w-20 hover:bg-gray-400 transition duration-100 align-center border border-gray-400 bg-gray-300 rounded-lg font-medium">accept</button>
+                    </div>
+                  </div>
+                  
+              </div>
+                    
+
                         
                     </div>
+
+                    
                 </div>
             </div>
+
+
 
             
             
         </div>
+        
 
     )
     

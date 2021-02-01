b1f4b75d9395578f14c078eb8d7ade8e29656250
johar-arjit-216490609 <arjit39@my.yorku.ca>
2021-02-01 12:18:42 -0500

    Update index.tsx

--- src/views/LandingPage/index.tsx
+++ src/views/LandingPage/index.tsx
@@ -31,22 +31,27 @@ export const Landing = () => {
 
 
   //implement function to close modal
+  //to do list
 
 
   //not using React boostrap, using display html instead to show Modal
 
   return (
     <div>
-      <div className="bg-gray-300 min-h-screen flex flex-col p-8 justify-center">
-        <div className="font-extrabold text-center text-lg"> Music Tab</div>
+      <div className="bg-purple-200 p-5">
+              stuff
+      </div>
+
+      <div className="min-h-screen flex  p-8 justify-center" style={{backgroundColor:'#171a1c'}}>
+        <div className="font-extrabold text-center text-lg " style={{color: 'white'}}> Music Tab</div>
         <div className="container max-w-md w-full mx-auto p-32 text-center border border-gray-50 background bg-white bg">
 
         </div>
         <div className="max-w-md w-full mx-auto">
           <div className="flex md:space-x-2 justify-center">
-            <button className="w-15 hover:bg-green-400 transition duration-100 align-center border border-green-400 bg-green-300 rounded-lg font-medium px-4">import</button>
+            <button className="w-15 hover:bg-green-400 transition duration-100  align-center border border-green-400 bg-green-300 rounded-lg font-medium px-4 ">import</button>
             <button className="w-15 hover:bg-blue-400 transition duration-100 align-center border border-blue-400 bg-blue-300 rounded-lg font-medium px-4" onClick={toggleModal}>paste</button>
-            <button className="w-15 hover:bg-red-400 transition duration-100 align-center border border-red-400 bg-red-300 rounded-lg font-medium px-4">reset</button>
+            <button className="w-15 hover:bg-red-400 transition duration-100 align-center border border-red-400 bg-red-300 rounded-lg font-medium px-4 ">reset</button>
             <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center" style={(showModal) ? { display: "" } : { display: "None" }}>
               
               <div className=" bg-gray-200 p-30">
@@ -62,16 +67,9 @@ export const Landing = () => {
                 <div className="mt=3 flex justify-end space-x-3">
                     <button onClick={accept} className="w-20 hover:bg-gray-400 transition duration-100 align-center border border-gray-400 bg-gray-300 rounded-lg font-medium">accept</button>
                   </div>
-
               </div>
-
             </div>
-
-
-
           </div>
-
-
         </div>
       </div>
       </div>

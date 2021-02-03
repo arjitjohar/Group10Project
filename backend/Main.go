4d2db9341e2ce28efc1b1ee4a796f8c1a853ea9c
Andrew <ngov.andrew@gmail.com>
2021-02-03 02:31:34 -0500

    Tablature Reader
    Reads the tablature and stores them in an array to be converted into sheet music.

--- backend/Main.go
+++ backend/Main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -17,6 +18,9 @@ func main() {
 
 	//Reads file line-by-line
 	noteList := make([]string, 0)         //Create a slice(Similar to dynamic array)
+	noteNotation := make([]string, 0)     //Stores the note notations on standard staff
+	noteNum := make([]string, 0)          //Stores the amount of notes per line
+	notePlayed := make([]string, 0)       //Stores the notes that are to be played
 	scanner := bufio.NewScanner(os.Stdin) //Scanner
 
 	//Scans through lines inputted by user and stores them in a slice
@@ -33,6 +37,13 @@ func main() {
 		//Checks lines that contains dashes
 		if strings.ContainsAny("-", noteList[i]) {
 			//TODO: implement conversion into sheet music
+			for j := i; j < i+7; j++ {
+				noteNotation = append(noteNotation, noteList[j])//NEED TO GET FIRST CHARACTER) //Stores note notation on staff
+				re := regexp.MustCompile("[hps0-9]+")            //Regex of golang(search for characters h, p, s and numbers 0-9)
+				//NOTENUM MAY NOT BE REQUIRED
+				noteNum = append(noteNum, re.FindAllStringIndex(noteList[j], -1)  //Counts the number of notes played on the current line
+				notePlayed = append(notePlayed, re.FindAllString(noteList[j], -1)) //Stores which are to be played in the current line
+			}
 		}
 	}
 }

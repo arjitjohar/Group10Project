9e3c50a6abc3be9be1fffb9356b93e0ef4fd6177
Alp Sirek <71943217+hiimangel@users.noreply.github.com>
2021-02-16 11:23:34 -0800

    Login and Signup update
    Log in and Sign up is on the right as buttons but for later use, we might just want to use href <a> for signing up and loging in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,48 +11,52 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <div>
+            
             <div className="relative bg-gray-800">
                     <div className="sm:px-8">
-                        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-                           
-                            <a className="text-base font-bold text-gray-500 hover:text-gray-900">
-                                <img className="h-10"src={group} alt="Group 10 Logo" ></img>
-                                <Link to="/">Home</Link>
-                            </a>
+                        <div>
+                           <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                                <a>
+                                    <img className="h-10"src={group} alt="Group 10 Logo" ></img>
+                                </a>
 
-                            <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                    <Link to="/">Home</Link>
+                                </a>
+
+                                <a className="text-base font-medium text-gray-500 hover:text-gray-900">
                                     <Link to="/help">Help</Link>
-
-                            </a>
-
+                                </a>
                             
-                            <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                <a className="text-base font-medium text-gray-500 hover:text-gray-900">
                                 
                                     <Link to="/about">about</Link>
-                            </a>
-                            
-                            <button className="border border-gray-900 bg-gray-700 text-white rounded-md px-4 py-2 m-2 hover:bg-gray-800 transition duration-500 ease">
-                                Log In
-                            </button>
+                                </a>
 
-                            <button  className="border border-gray-900 bg-gray-700 text-white rounded-md px-4 py-2 m-2 hover:bg-gray-800 transition duration-500 ease">
-                                Sign Up
-                            </button>
 
-                            <a className="hidden md:flex items-center justify-end md:flex-1 lg:w-0" href="https://github.com/arjitjohar/Group10Project">
-                                <img src={logo}  alt="GitHub">
+                              
+                                <div  className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
 
-                                </img>
-                            </a>
+                                    <button className="border border-gray-900 bg-gray-700 text-white rounded-md px-4 py-2 m-2 hover:bg-gray-800 transition duration-500 ease ">
+                                        Log In
+                                    </button>
 
-                          
-                           
-                            
-                        </div>
+                                    <button  className="border border-gray-900 bg-gray-700 text-white rounded-md px-4 py-2 m-2 hover:bg-gray-800 transition duration-500 ease">
+                                        Sign Up
+                                    </button>
+                                    
+                                    <a  href="https://github.com/arjitjohar/Group10Project">
+                                        <img src={logo}  alt="GitHub"></img>
+                                     </a>
+
+                                </div>
+
+                                
+                            </div>
                     </div>
                 </div>
             </div>
+            
         )
 
 

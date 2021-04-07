0645b2fd62c29ca9f0594c93dc536e9621083112
dsantor9 <dannyboy9901@rogers.com>
2021-04-07 11:38:21 -0400

    isDrums moved to Main.py + Edited

--- Tabs2XML/main.py
+++ Tabs2XML/main.py
@@ -95,7 +95,16 @@ def isUnrecognizedCharacter(tab):
         return problem
 
       
-
+    def isDrums(text):    #MOVED FROM CONVERSION ENGINE - WORK IN PROGRESS - 1=Guitar, 2=Bass, 3=Drums
+        finaltype = 1
+      #  isdrumscounter = 0
+       # for i in text:
+        #    if text(i[0]) != "|" and text(i[1]) != "|" and text(i[3]) == "|":
+     #           isdrumscounter ++
+      #  if isdrumscounter == text.length():
+     #       drums = True
+        
+        return finaltype
 
 varTimeSig = ["1/4", "2/4", "3/4", "4/4"]
 
